ac8c6a44f65d062c1a2710af9f2a4db922e425d4
Max Brauer <ma.brauer@live.de>
2020-07-25 17:39:43 +0200

    add a NuGet package for sRPCgen with the Name sRPC.Tools. Also move the version management to a single file.

---
+++ sRPC.Tools/Metadata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sRPC.Tools
+{
+    static class Metadata
+    {
+        public const string NamespaceBase = "NamespaceBase";
+        public const string SrpcExt = "SrpcExt";
+        public const string ProtoExt = "ProtoExt";
+        public const string ProtoCompile = "ProtoCompile";
+    }
+}
---
+++ sRPC.Tools/ProtoPlatform.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace sRPC.Tools
+{
+    public class ProtoPlatform : Task
+    {
+        [Output]
+        public string Os { get; set; }
+
+        [Output]
+        public string Cpu { get; set; }
+
+        public override bool Execute()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                Os = "windows";
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                Os = "linux";
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                Os = "maxosx";
+            else Os = "";
+
+            switch (RuntimeInformation.ProcessArchitecture)
+            {
+                case Architecture.X86: Cpu = "x86"; break;
+                case Architecture.X64: Cpu = "x64"; break;
+                default: Cpu = ""; break;
+            }
+
+            return true;
+        }
+    }
+}
